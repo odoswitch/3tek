7bb8a8e2b8436699d5ae97355c2ee4c38e805713
unknown <dng@afritelec.fr>
2025-06-15 16:55:21 +0200

    image ok

--- src/Controller/Admin/LotCrudController.php
+++ src/Controller/Admin/LotCrudController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Lot;
 use Doctrine\DBAL\Types\FloatType;
+use Symfony\Component\DomCrawler\Image;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -23,10 +25,11 @@ class LotCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            //IdField::new('id'),
+            
             TextField::new('name'),
             TextEditorField::new('description'),
-            TextField::new('image'),
+            ImageField::new('image')->setBasePath('public/uploads/images/')->setUploadDir('public/uploads/images/'),
+            //->setUploadedFileNamePattern('[year]/[month]/[day].[extension]'),
             NumberField::new('prix'),
             AssociationField::new('cat')->autocomplete(),
             AssociationField::new('types'),
