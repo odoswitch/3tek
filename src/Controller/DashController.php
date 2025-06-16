5ce3e9c6db30405162c797b1072983858b40fd8a
unknown <dng@afritelec.fr>
2025-06-16 09:59:40 +0200

    getuser

--- src/Controller/DashController.php
+++ src/Controller/DashController.php
@@ -2,22 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Lot;
 use App\Repository\LotRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class DashController extends AbstractController
 {
     #[Route('/dash', name: 'app_dash')]
-    public function index(LotRepository $lot): Response
+    public function index(LotRepository $lot, Lot $l): Response
     {
-        $data = $lot->findAll();
-        //dd($data);
+        $user = $this->getUser()->getid();
+
+        if($this->isGranted('ROLE_ADMIN')){
+            $data = $lot->findAll();
+            return $this->render('dash1.html.twig', [
+
+                'controller_name' => 'DashController',
+                'data' => $data,
+            ]);
+
+
+       }else{
+        $data = $lot->lotUser($user);
+       
         return $this->render('dash1.html.twig', [
 
             'controller_name' => 'DashController',
             'data' => $data,
         ]);
+
+       }
+       
+        
     }
 }
--- src/Repository/LotRepository.php
+++ src/Repository/LotRepository.php
@@ -16,6 +16,15 @@ class LotRepository extends ServiceEntityRepository
         parent::__construct($registry, Lot::class);
     }
 
+    public function lotUser($a){
+       // $a = 2;
+       $sql = 'SELECT * FROM lot INNER JOIN user on lot.id = user.lot_id WHERE user.id = '.$a ;
+       // $sql = 'SELECT * FROM lot INNER JOIN user on lot.id = user.lot_id WHERE user.id = '.$a .'AND user.is_verified = 0';
+        $con = $this->getEntityManager()->getConnection();
+        $resultat = $con->query($sql);
+        return $resultat->fetchAllAssociative();
+    }
+
 
 
     //    public function findOneBySomeField($value): ?Lot
