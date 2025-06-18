d36a21f05422802a2dd90afb7b73fae2b4f7b1a3
unknown <dng@afritelec.fr>
2025-06-18 15:58:41 +0200

    email verify ok

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationForm;
 use App\Security\EmailVerifier;
+use Symfony\Component\Mime\Email;
+use App\Repository\UserRepository;
 use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
@@ -47,7 +49,7 @@ class RegistrationController extends AbstractController
                 ->replyTo('info@odoip.fr')
                 ->subject('NGAMBA TEST MAIl')
                 ->text('Bonjour si vous avez reçu ce message cela veut dire tout est ok pour enregistrement')
-                ->html('<h2>Bonjour si vous avez reçu ce message cela veut dire tout est ok pour vous sur le partir dsn</h2>');
+                ->html('<h2>Bonjour si vous avez reçu ce message cela veut dire tout est ok pour vous sur la partir dsn</h2>');
             $mailer->send($email);
 
             // generate a signed url and email it to the user
@@ -55,7 +57,7 @@ class RegistrationController extends AbstractController
                 'app_verify_email',
                 $user,
                 (new TemplatedEmail())
-                    ->from(new Address('info@odoip.fr', 'Tek-Europe'))
+                    ->from(new Address('info@odoip.fr', '3Tek-Europe'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -73,14 +75,30 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
+    public function verifyUserEmail(Request $request, TranslatorInterface $translator, UserRepository $userRepository): Response
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+       // $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+       $id = $request->query->get('id');
+       //dd($id);
+       // Verify the user id exists and is not null
+  if (null === $id) {
+           return $this->redirectToRoute('app_login');
+       }
+
+       $user = $userRepository->find($id);
+
+       // Ensure the user exists in persistence
+       if (null === $user) {
+           return $this->redirectToRoute('app_login');
+       }
 
         // validate email confirmation link, sets User::isVerified=true and persists
         try {
             /** @var User $user */
-            $user = $this->getUser();
+            //$this->verifyEmailHelper->validateEmailConfirmationFromRequest($request, $user->getId(), $user->getEmail());
+            //$user = $this->getUser();
+            //dd($user);
             $this->emailVerifier->handleEmailConfirmation($request, $user);
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
@@ -91,6 +109,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
